c70398f986fed3a42aabf0decb8fe172f888eb58
Christophe Lecointe <christophe.lecointe@opmbx.org>
2015-09-20 17:10:07 +0200

    Alpha, still needs to test everything
    Signed-off-by: Christophe Lecointe <christophe.lecointe@opmbx.org>

--- discovery/serf/serf.go
+++ discovery/serf/serf.go
@@ -1,15 +1,16 @@
 package serf
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
-	"time"
+  "encoding/json"
+  "errors"
+  "fmt"
+  "io/ioutil"
+  "net/http"
+  "strings"
+  "time"
+  "os/exec"
 
-	"github.com/docker/swarm/discovery"
+  "github.com/docker/swarm/discovery"
 )
 
 // DiscoveryUrl is exported
@@ -17,97 +18,110 @@ const DiscoveryURL = "https://discovery.hub.docker.com/v1"
 
 // Discovery is exported
 type Discovery struct {
-	heartbeat time.Duration
-	ttl       time.Duration
-	url       string
-	token     string
+  heartbeat time.Duration
+  ttl       time.Duration
+  url       string
+  token     string
 }
 
 func init() {
-	Init()
+  Init()
 }
 
 // Init is exported
 func Init() {
-	discovery.Register("serf", &Discovery{})
+  discovery.Register("serf", &Discovery{})
 }
 
 // Initialize is exported
 func (s *Discovery) Initialize(ip_port string, heartbeat time.Duration, ttl time.Duration) error {
   s.ip_port = ip_port
   s.heartbeat = heartbeat
-	s.ttl = ttl
+  s.ttl = ttl
 
-  // Launch the agent in standby maybe?
+  // Launch the agent in standby
+  err := exec.Command("./launch_agent.sh").Run()
+  if err != nil {
+    return err
+  }
 
-	return nil
+  return nil
 }
 
 // Fetch returns the list of entries for the discovery service at the specified endpoint
 func (s *Discovery) fetch() (discovery.Entries, error) {
   // Here, we contact the serf agent and ask for a list of members
   // We have to send back entries, which are basically a host and a port (see discovery.go)
+  output, err := exec.Command("./agent_members.sh").Output()
 
-	return discovery.CreateEntries(addrs)
+  if err != nil {
+    return nil, err
+  }
+
+
+  lines = strings.Split(output, "\n")
+  var addrs []string
+  for line := range fields {
+    fields := strings.Fields(line)
+    if len(field) != 3 {
+      return nil, errors.New("Error while parsing the output of serf members : Wrong number of fields")
+    }
+    append(addrs, field[1])
+  }
+
+  return discovery.CreateEntries(addrs)
 }
 
 // Watch is exported
 func (s *Discovery) Watch(stopCh <-chan struct{}) (<-chan discovery.Entries, <-chan error) {
-	ch := make(chan discovery.Entries)
-	ticker := time.NewTicker(s.heartbeat)
-	errCh := make(chan error)
+  ch := make(chan discovery.Entries)
+  ticker := time.NewTicker(s.heartbeat)
+  errCh := make(chan error)
 
-	go func() {
-		defer close(ch)
-		defer close(errCh)
+  go func() {
+    defer close(ch)
+    defer close(errCh)
 
-		// Send the initial entries if available.
-		currentEntries, err := s.fetch()
-		if err != nil {
-			errCh <- err
-		} else {
-			ch <- currentEntries
-		}
+    // Send the initial entries if available.
+    currentEntries, err := s.fetch()
+    if err != nil {
+      errCh <- err
+    } else {
+      ch <- currentEntries
+    }
 
-		// Periodically send updates.
-		for {
-			select {
-			case <-ticker.C:
-				newEntries, err := s.fetch()
-				if err != nil {
-					errCh <- err
-					continue
-				}
+    // Periodically send updates.
+    for {
+      select {
+      case <-ticker.C:
+        newEntries, err := s.fetch()
+        if err != nil {
+          errCh <- err
+          continue
+        }
 
-				// Check if the file has really changed.
-				if !newEntries.Equals(currentEntries) {
-					ch <- newEntries
-				}
-				currentEntries = newEntries
-			case <-stopCh:
-				ticker.Stop()
-				return
-			}
-		}
-	}()
+        // Check if the file has really changed.
+        if !newEntries.Equals(currentEntries) {
+          ch <- newEntries
+        }
+        currentEntries = newEntries
+      case <-stopCh:
+        ticker.Stop()
+        return
+      }
+    }
+  }()
 
-	return ch, errCh
+  return ch, errCh
 }
 
 // Register adds a new entry identified by the into the discovery service
 func (s *Discovery) Register(addr string) error {
-  // We need to launch the agent with the correct configuration here
-  // Exec ?
+  // Launch the agent in standby
+  err := exec.Command("./agent_join.sh", s.ip_port).Run()
+  if err != nil {
+    return err
+  }
 
-	buf := strings.NewReader(addr)
-
-	resp, err := http.Post(fmt.Sprintf("%s/%s/%s?ttl=%d", s.url,
-		"clusters", s.token, uint64(s.ttl.Seconds())), "application/json", buf)
-
-	if err != nil {
-		return err
-	}
-
-	resp.Body.Close()
-	return nil
+  return nil
 }
